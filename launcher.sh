73af60c916404b870c202644d2f45274fc3f7f2c
Yajo <yajo@openaliasbox.org>
2014-09-09 13:22:33 +0200

    Addons must be separated by comma, without spaces in between nor trailing comma.

--- launcher.sh
+++ launcher.sh
@@ -11,8 +11,8 @@ DB_ENV_POSTGRESQL_DB=${DB_ENV_POSTGRESQL_DB:-"docker"}
 if [ -f /firstrun ]; then
     echo Patching configuration on first run
 
-    addons=$(ls -1bdm /usr/local/lib/python2.7/dist-packages/openerp/addons \
-                      /opt/odoo/extra-addons/*)
+    addons=$(ls -1bd /usr/local/lib/python2.7/dist-packages/openerp/addons \
+                     /opt/odoo/extra-addons/* | tr '\n' ',' | sed s/,$//)
 
     echo "
 [options]
