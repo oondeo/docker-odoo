c3df4b3e8fcf3a5fe7d6db6d911d91d201187430
AmonRa <amon.raj@gmail.com>
2016-11-30 19:24:49 +0100

    Changed scripts, now suports installs from dir. Tested

--- onbuild/build.sh
+++ onbuild/build.sh
@@ -1,10 +1,14 @@
 #!/sh
 
 export ODOO_MODULES="https://github.com/OCA/l10n-spain/archive/8.0.zip"
+
 set -e
 
+export > /etc/skel/initrc
 /usr/local/bin/odoo-install
 rm -rf $ODOO_HOME/doc $ODOO_HOME/setup* $ODOO_HOME/debian
 cd /opt
-$PYTHON_BIN  -m compileall .
+$PYTHON_BIN -m compileall .
+cd /usr/lib/python2.7/site-packages && python -m compileall .
+install-deps /opt /usr/lib/python2.7/site-packages
 /usr/local/bin/clean.sh
--- onbuild/scripts/clean.sh
+++ onbuild/scripts/clean.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 rm -rf $ODOO_HOME/doc $ODOO_HOME/setup* $ODOO_HOME/debian
-find /opt -name "*.py" -exec rm -f {} \;
+find /usr/lib/python2.7 -name "*.py" -and -not -name "__*" -exec rm -f {} \;
+find /opt -name "*.py" -and -not -name "__*" -exec rm -f {} \;
 npm uninstall -g less less-plugin-clean-css
-gem uninstall -x -q sass
+gem uninstall -x -q -f compass
+gem uninstall -x -q -f sass
 apk del --no-cache .builddeps
 rm -rf /usr/lib/node_modules
 rm -rf /usr/lib/ruby
