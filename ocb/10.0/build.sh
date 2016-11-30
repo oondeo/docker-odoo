4c595ced25e8babf912733a0608f3d11b9f0931e
AmonRa <amon.raj@gmail.com>
2016-11-30 20:10:05 +0100

    New versions, now tested

---
+++ ocb/10.0/build.sh
@@ -0,0 +1,19 @@
+#!/sh
+
+export ODOO_VERSION="10.0"
+export ODOO_MODULES="" \
+  ODOO_SERVER="$PYTHON_BIN odoo-bin" \
+  ODOO_MODULE_FILE="__manifest__.pyc" \
+  ODOO_URL="https://github.com/OCA/OCB/archive/$ODOO_VERSION.zip" \
+  ODOO_TARBALL_DIR="OCB-$ODOO_VERSION" 
+
+set -e
+
+export > /etc/skel/initrc
+/usr/local/bin/odoo-install
+rm -rf $ODOO_HOME/doc $ODOO_HOME/setup* $ODOO_HOME/debian
+cd /opt
+$PYTHON_BIN -m compileall .
+cd /usr/lib/python2.7/site-packages && python -m compileall .
+install-deps /opt /usr/lib/python2.7/site-packages
+/usr/local/bin/clean.sh
---
+++ ocb/8.0-dev/build.sh
@@ -0,0 +1,27 @@
+#!/sh
+
+export ODOO_MODULES="https://github.com/oondeo/git-install/archive/8.0.zip"
+export PHANTOMJS_VERSION=2.11
+export  PYTHON_MODULES="wdb pyinotify openupgradelib flanker odoo_gateway"
+export WDB_NO_BROWSER_AUTO_OPEN=True \
+    WDB_SOCKET_SERVER=wdb \
+    WDB_WEB_PORT=1984 \
+    WDB_WEB_SERVER=localhost
+set -e
+
+export > /etc/skel/initrc
+
+apk add --no-cache -t .rundeps fontconfig && \
+  mkdir -p /usr/share && \
+  cd /usr/share \
+  && curl -L https://github.com/Overbryd/docker-phantomjs-alpine/releases/download/$PHANTOMJS_VERSION/phantomjs-alpine-x86_64.tar.bz2 | tar xj \
+  && ln -s /usr/share/phantomjs/phantomjs /usr/bin/phantomjs
+
+$PIP_BIN --no-cache-dir install $PYTHON_MODULES
+/usr/local/bin/odoo-install
+
+install-deps /opt /usr/lib/python2.7/site-packages
+cd /opt
+$PYTHON_BIN -m compileall .
+cd /usr/lib/python2.7/site-packages && python -m compileall .
+rm -rf $ODOO_HOME/doc $ODOO_HOME/setup* $ODOO_HOME/debian
