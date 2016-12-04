b6eed5b2ddb9156043343d7cd0778b7bc6f76f13
AmonRa <amon.raj@gmail.com>
2016-12-04 21:57:30 +0100

    Change to use debian base

--- onbuild/scripts/clean.sh
+++ onbuild/scripts/clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 rm -rf $ODOO_HOME/doc $ODOO_HOME/setup* $ODOO_HOME/debian
-find /usr/lib/python2.7 -name "*.py" -and -not -name "__*" -exec rm -f {} \;
+find $PYTHON_DIR/site-packages "*.py" -and -not -name "__*" -exec rm -f {} \;
 find /opt -name "*.py" -and -not -name "__*" -exec rm -f {} \;
 npm uninstall -g less less-plugin-clean-css
 gem uninstall -x -q -f compass
