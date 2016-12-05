943e94f7c4dd5eeb378e6effc9f4300128fb6826
AmonRa <amon.raj@gmail.com>
2016-12-05 17:25:17 +0100

    FIX PYTHON_DIR and PIP_DIR

--- onbuild/scripts/clean.sh
+++ onbuild/scripts/clean.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 rm -rf $ODOO_HOME/doc $ODOO_HOME/setup* $ODOO_HOME/debian
-find $PYTHON_DIR/site-packages "*.py" -and -not -name "__*" -exec rm -f {} \;
+find $PYTHON_DIR "*.py" -and -not -name "__*" -exec rm -f {} \;
+find $PIP_DIR "*.py" -and -not -name "__*" -exec rm -f {} \;
 find /opt -name "*.py" -and -not -name "__*" -exec rm -f {} \;
 npm uninstall -g less less-plugin-clean-css
 gem uninstall -x -q -f compass
